990c4a41104eeec1bf6142d9e6abe7b590001c45
mgurga <motiurga@gmail.com>
2024-07-19 12:20:56 -0700

    draw game border and lanes

--- src/Game.ts
+++ src/Game.ts
@@ -1,9 +1,69 @@
+import { bgctx, gamectx, bgcanvas, gamecanvas, gw, gh, bgw, bgh } from "./main";
 import { Page } from "./types";
+import { ColorScheme } from "./ColorScheme"
+import { hexToRGB } from "./Utils";
+
+const LANES: number = 7;
 
 export class Game implements Page {
+
     next_page: Page | null = null;
+    tick: number = 0;
+    cs: ColorScheme;
 
-    constructor() { }
+    constructor() {
+        this.cs = new ColorScheme();
+    }
 
-    draw() { }
+    draw() {
+        gamecanvas.style.filter = "grayscale(1)";
+        bgcanvas.style.filter = "grayscale(1)";
+
+        gamectx.fillStyle = this.cs.bg;
+        gamectx.fillRect(0, 0, gw, gh);
+
+        bgctx.fillStyle = this.cs.bg;
+        bgctx.fillRect(0, 0, bgw, bgh);
+
+        if (this.tick < 100) {
+            for (let i = 0; i < Math.floor(this.tick / 20); i++) {
+                let c = hexToRGB(this.cs.fg);
+                gamectx.strokeStyle = `rgb(${c[0] - (i * 10)}, ${c[1] - (i * 10)}, ${c[2] - (i * 10)})`;
+
+                gamectx.lineWidth = 3;
+                gamectx.beginPath();
+                gamectx.roundRect(i * 2, i * 2, gw - (i * 4), gh - (i * 4), 5);
+                gamectx.stroke();
+            }
+
+            this.tick++;
+            return;
+        }
+
+        this.draw_lanes();
+        this.draw_border();
+
+        this.tick++;
+    }
+
+    draw_lanes() {
+        gamectx.fillStyle = this.cs.am;
+        for (let l = 1; l < LANES; l++) {
+            for (let d = 0; d < gh / 30; d++) {
+                gamectx.fillRect(15 + (l * ((gw - 30) / LANES)), d * 60 + ((this.tick / 2) % 60) - 30, 2, 30);
+            }
+        }
+    }
+
+    draw_border() {
+        for (let i = 0; i < 5; i++) {
+            let c = hexToRGB(this.cs.fg);
+            gamectx.strokeStyle = `rgb(${c[0] - (i * 10)}, ${c[1] - (i * 10)}, ${c[2] - (i * 10)})`;
+
+            gamectx.lineWidth = 3;
+            gamectx.beginPath();
+            gamectx.roundRect(i * 2, i * 2, gw - (i * 4), gh - (i * 4), 5);
+            gamectx.stroke();
+        }
+    }
 }
--- src/Utils.ts
+++ src/Utils.ts
@@ -1,4 +1,4 @@
-import { gw, gh } from "./main.ts";
+import { gw, gh } from "./main";
 
 // game canvas width percentage
 export function gwp(p: number): number { return gw * (p / 100); }
@@ -14,4 +14,13 @@ export function getImageElement(data: string): HTMLImageElement {
 
 export function rainbow(i: number): string {
     return "hsl(" + (i % 360) + " , 100%, 50%)";
+}
+
+export function hexToRGB(hex: string) {
+    let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)!;
+    let r: number = parseInt(result[1], 16);
+    let g: number = parseInt(result[2], 16);
+    let b: number = parseInt(result[3], 16);
+    // r /= 255, g /= 255, b /= 255;
+    return [r, g, b];
 }
