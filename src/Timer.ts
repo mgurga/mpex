58b20d9e68e1a400c40c8135969c052420a324a3
mgurga <motiurga@gmail.com>
2024-08-12 20:58:38 -0700

    fixed bug where timer became negative

--- src/Timer.ts
+++ src/Timer.ts
@@ -20,6 +20,9 @@ export class Timer implements Drawable {
         gamectx.fillStyle = `rgba(${rgb[0]}, ${rgb[1]}, ${rgb[2]}, 0.4)`;
         gamectx.fillText(this.timer + "", x, y);
 
-        if (this.timer == 0) clearInterval(this.timer_interval);
+        if (this.timer <= 0) {
+            clearInterval(this.timer_interval);
+            this.timer = 0;
+        }
     }
 }
